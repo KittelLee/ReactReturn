26a6d8129a66236441a6a438ef5838a30bece6ae
Leejinuk <leejin7900@gmail.com>
2024-05-18 21:57:00 +0900

    Feat: 기능구현

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   let post = '강남 우동 맛집'
 
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
   let [따봉, 따봉변경] = useState(0)
   let [clothes, setClothes] = useState('남자코트 추천')
 
@@ -14,6 +14,13 @@ function App() {
       <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={ ()=>{ 
+      let copy = [...글제목];
+      copy.sort();
+      글제목변경(copy)
+    } }> 정렬버튼 </button>
+
       <div className='list'>
         <h4>{글제목[0]} <span onClick={()=> {따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
@@ -34,7 +41,18 @@ function App() {
         <span onClick={() => {setClothes('여자 코트 추천')}}>❗️추천변경하기</span>
         <p>2월 17일 발행</p>
       </div>
+      <Modal/>
     </div>
+  )
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
   )
 }
 
