aa7f57535b833515c63bc1861c433e3229b4c739
Leejinuk <leejin7900@gmail.com>
2024-08-28 17:45:43 +0900

    Feat: props study

--- src/App.js
+++ src/App.js
@@ -88,17 +88,18 @@ function App() {
         </span>
         <p>2월 17일 발행</p>
       </div>
-      {modal == true ? <Modal /> : null}
+      {modal == true ? <Modal 글제목={글제목} /> : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   );
 }
