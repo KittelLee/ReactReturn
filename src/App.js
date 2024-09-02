53b8741033ce4a06bc0633fc6232274e007981e1
Leejinuk <leejin7900@gmail.com>
2024-09-02 16:25:11 +0900

    Feat: props로 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState([
-    "남자 코트 추천",
-    "강남 우동맛집",
-    "파이썬독학",
-  ]);
-  let [따봉, 따봉변경] = useState([0, 0, 0]);
-  let [clothes, setClothes] = useState("남자코트 추천");
-  let [pen, setPen] = useState("모나미");
+  let [글제목] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   [1, 2, 3].map(function (a) {
     return "1233211";
@@ -22,71 +17,53 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy.sort();
-          글제목변경(copy);
-        }}
-      >
-        {" "}
-        정렬버튼{" "}
-      </button>
-
-      <div className="list">
-        <h4>{글제목[1]} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
       {글제목.map(function (a, i) {
         return (
           <div className="list" key={i}>
-            <h4>
+            <h4
+              onClick={() => {
+                setModal(true);
+                setTitle(i);
+              }}
+            >
               {글제목[i]}
               <span
                 onClick={() => {
-                  let copy = [...따봉];
-                  copy[i] = copy[i] + 1;
-                  따봉변경(copy);
+                  따봉변경(따봉 + 1);
                 }}
               >
                 👍
               </span>
-              {따봉[i]}
+              {따봉}
             </h4>
             <p>2월 17일 발행</p>
           </div>
         );
       })}
 
-      <div className="list" style={{ paddingBottom: "10px" }}>
-        <h4>{pen}</h4>
-        <span onClick={() => setPen("제트스트림")}>클릭시 변경</span>
-      </div>
+      <button
+        onClick={() => {
+          setTitle(0);
+        }}
+      >
+        글제목0
+      </button>
+      <button
+        onClick={() => {
+          setTitle(1);
+        }}
+      >
+        글제목1
+      </button>
+      <button
+        onClick={() => {
+          setTitle(2);
+        }}
+      >
+        글제목2
+      </button>
 
-      <div className="list">
-        <h4>{clothes}</h4>
-        <span
-          onClick={() => {
-            setClothes("여자 코트 추천");
-          }}
-        >
-          ❗️추천변경하기
-        </span>
-        <p>2월 17일 발행</p>
-      </div>
-      {modal == true ? <Modal 글제목변경={글제목변경} 글제목={글제목} /> : null}
+      {modal == true ? <Modal title={title} 글제목={글제목} /> : null}
     </div>
   );
 }
@@ -94,16 +71,10 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h4>{props.글제목[0]}</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button
-        onClick={() => {
-          props.글제목변경(["여자코트 추천", "강남우동맛집", "파이썬독학"]);
-        }}
-      >
-        글수정
-      </button>
+      <button>글수정</button>
     </div>
   );
 }
